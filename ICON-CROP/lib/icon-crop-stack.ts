8c07c128a6aaa1d54b39d24f2acc4e49fef7559b
Shadid Sayeed <shadid_sayeed@hotmail.com>
2025-10-03 05:13:51 +0000

    Add bucket policies

--- ICON-CROP/lib/icon-crop-stack.ts
+++ ICON-CROP/lib/icon-crop-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { createIconBuckets } from './s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,7 @@ export class IconCropStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'IconCropQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const { iconSource, iconDest } = createIconBuckets(this);
+    
   }
 }
--- ICON-CROP/lib/s3.ts
+++ ICON-CROP/lib/s3.ts
@@ -1,9 +1,13 @@
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export function createIconBuckets(scope:Construct): { iconSource: s3.Bucket, iconDest: s3.Bucket } {
 
-    const iconSource = new s3.Bucket(scope, 'iconSource');
+    const iconSource = new s3.Bucket(scope, 'iconSource', {
+        removalPolicy: RemovalPolicy.DESTROY,
+    });
 
     const iconDest = new s3.Bucket(scope, 'iconDest', {
         blockPublicAccess: new s3.BlockPublicAccess({
@@ -13,9 +17,16 @@ export function createIconBuckets(scope:Construct): { iconSource: s3.Bucket, ico
             restrictPublicBuckets: false,
           }),
         publicReadAccess: true,
+        removalPolicy: RemovalPolicy.DESTROY,
         
     });
     
+    iconDest.addToResourcePolicy(new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [`${iconDest.bucketArn}/*`],
+        principals: [new iam.AnyPrincipal()],
+        effect: iam.Effect.ALLOW,
+      }));
     
     return {iconSource, iconDest}
 }
