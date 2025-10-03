3cef8a271b00a5f7c250960c7cd2408302510b13
Shadid Sayeed <shadid_sayeed@hotmail.com>
2025-10-03 01:00:22 +0000

    Initial files for icon-crop app

---
+++ ICON-CROP/test/icon-crop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IconCrop from '../lib/icon-crop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/icon-crop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IconCrop.IconCropStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
